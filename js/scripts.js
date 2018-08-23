c0da240a56821007f9134a90a7d67365a4a3d6a8
Hassan Al-khalifah and Liza Tennis <liza@>
2018-08-23 16:44:42 -0700

    create starting function to check if player is winner

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,69 @@
 //Business Logic
-function Players(player) {
+function Players(player, mark) {
   this.player = player,
-  this.active = false
+  this.active = false,
+  this.mark = mark
+}
+function checkForWin(currentChoice) {
+  var possibleWins = [
+    ["A1","A2","A3"],
+    ["B1","B2","B3"],
+    ["C1","C2","C3"],
+    ["A1","B1","C1"],
+    ["A2","B2","C2"],
+    ["A3","B3","C3"],
+    ["A3","B2","C1"],
+    ["A1","B2","C3"]
+  ];
+
+  // for(var i = 0; i < possibleWins.length; i++) {
+  //   var possibleWinSet = possibleWins[i];
+  //   for(var j = 0; j < count.length; j++) {
+  //     var possibleWinEachPosition = possibleWinSet[j];
+  //     for(var y = 0; y < currentChoice.length; y++) {
+  //       if(currentChoice[y] == possibleWinEachPosition) {
+  //         console.log("Match");
+  //       } else {
+  //         console.log("No match");
+  //       }
+  //     }
+  //   }
+  // }
 }
 var players = [];
+var player1Choices = [];
+var player2Choices = [];
 //User Interface Logic
 $(document).ready(function() {
-  var player1 = new Players("player 1");
+  var player1 = new Players("player 1", "x");
   player1.active = true;
-  var player2 = new Players("player 2");
+  var player2 = new Players("player 2", "o");
   players.push(player1,player2);
-  console.log(players);
   $(".box").click(function() {
     if($(this).css("background-image") === "none") {
       for(var i = 0; i < players.length; i++) {
         if(players[i].active === true) {
-          $(this).css({
-            "background-image" : "url('img/x.png')",
-            "background-size" : "cover"
-          });
-        } else {
-          console.log("Not your turn");
+          if(players[i].player === "player 1") {
+            $(this).css({
+              "background-image" : "url('img/x.png')",
+              "background-size" : "cover"
+            });
+            player1Choices.push($(this).attr("id"));
+            checkForWin(player1Choices);
+            players[i].active = false;
+          } else {
+            $(this).css({
+              "background-image" : "url('img/oh.png')",
+              "background-size" : "cover"
+            });
+            player2Choices.push($(this).attr("id"));
+            checkForWin(player2Choices);
+            players[i].active = false;
+          }
+        } else if (players[i].active === false) {
+          players[i].active = true;
         }
       }
-      // if(player.active === true) {
-      //   $(this).css({
-      //     "background-image" : "url('img/x.png')",
-      //     "background-size" : "cover"
-      //   });
-      // }
     }
   });
 });
